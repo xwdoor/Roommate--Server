cafaea89fadce7ebfe30ca22d4e71dfc41383620
xwdoor <xwdoor@126.com>
2016-03-25 17:28:47 +0800

    创建项目
    添加页面：获取所有账单、获取某用户的账单

---
+++ Server/getUserBill.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Server
+{
+    public partial class getUserBill : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string userId = Request.QueryString["userId"];
+            Response.Write("{\"userId "+userId+"的所有账单\"}");
+        }
+    }
+}
